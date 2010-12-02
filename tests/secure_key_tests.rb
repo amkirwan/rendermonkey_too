0cbc3c9b8389cd060d7593c30293457f8de56868
Anthony Kirwan <amkirwan@gmail.com>
2010-12-01 23:17:11 -0500

    fixed test from name to api_name

--- tests/secure_key_tests.rb
+++ tests/secure_key_tests.rb
@@ -1,3 +1,4 @@
+# needs to run first 
 ENV['RACK_ENV'] = "test"
 
 $LOAD_PATH << File.join(Dir.getwd, "..")
@@ -151,7 +152,7 @@ class SecureKeyTests < Test::Unit::TestCase
     signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('SHA256'), @hash_key, @sk.canonical_querystring)).chomp
     @params["signature"] = signature
 
-    @api = {"name" => "test_valid_login_api", 
+    @api = {"app_name" => "test_valid_api_secure_key", 
             "api_key" => "835a3161dc4e71b", 
             "hash_key" => "sQQTe93eWcpV4Gr5HDjKUh8vu2aNDOvn3+suH1Tc4P4="}
     @api_secure_key = ApiSecureKey.new(@api)
@@ -163,8 +164,6 @@ class SecureKeyTests < Test::Unit::TestCase
     @params = nil
     @api_secure_key.destroy
   end
-
-  private
 
   def edit_params(hash_key=nil, options={})
     @params.merge!(options)
@@ -182,43 +181,7 @@ class SecureKeyTests < Test::Unit::TestCase
     defaults.merge!(options)
     @api_secure_key.update(defaults)
   end
-  
- 
 
-  def setup
-    @sk = SecureKey::Digest.new
-    @params = {"timestamp" => "#{Time.now.utc.iso8601}", 
-               "page"=>"<b>Hello</b>, World!", 
-               "api_key"=>"835a3161dc4e71b"}
-               
-    @hash_key = "sQQTe93eWcpV4Gr5HDjKUh8vu2aNDOvn3+suH1Tc4P4=" 
-    @sk.canonical_querystring = @params
-    signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('SHA256'), @hash_key, @sk.canonical_querystring)).chomp
-    @params["signature"] = signature
-    
-    @api = {"app_name" => "test_valid_login_api", 
-            "api_key" => "835a3161dc4e71b", 
-            "hash_key" => "sQQTe93eWcpV4Gr5HDjKUh8vu2aNDOvn3+suH1Tc4P4="}
-    @api_secure_key = ApiSecureKey.new(@api)
-    @api_secure_key.save
-  end
-  
-  def teardown
-    @sk = nil
-    @params = nil
-    @api_secure_key.destroy
-  end
-  
-  private
-
-  def edit_params(hash_key=nil, options={})
-    @params.merge!(options)
-    if !hash_key.nil?
-      signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('SHA256'), hash_key, @params["page"])).chomp
-      @params["signature"] = signature
-    end
-    @params
-  end
   
   def update_api_secure_key(options={})
     defaults = {"app_name" => "test_valid_login_api", 
